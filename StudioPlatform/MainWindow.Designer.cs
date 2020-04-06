410f377c85a170e962ee635f8469d10bf5b84f05
Usama Tariq <usamat37@gmail.com>
2020-04-05 17:39:14 -0700

    Add project files.

---
+++ StudioPlatform/MainWindow.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace StudioPlatform
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBrowsers = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlWindow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlWindow
+            // 
+            this.pnlWindow.Controls.Add(this.pnlBrowsers);
+            this.pnlWindow.Size = new System.Drawing.Size(918, 547);
+            this.pnlWindow.Controls.SetChildIndex(this.pnlBrowsers, 0);
+            // 
+            // pnlBrowsers
+            // 
+            this.pnlBrowsers.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.pnlBrowsers.ColumnCount = 3;
+            this.pnlBrowsers.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBrowsers.Location = new System.Drawing.Point(0, 0);
+            this.pnlBrowsers.Name = "pnlBrowsers";
+            this.pnlBrowsers.RowCount = 3;
+            this.pnlBrowsers.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.pnlBrowsers.Size = new System.Drawing.Size(915, 547);
+            this.pnlBrowsers.TabIndex = 5;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 587);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Studio Platform";
+            this.TitleText = "Studio Platform";
+            this.pnlWindow.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel pnlBrowsers;
+    }
+}
+
---
+++ StudioPlatform/MainWindow.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using StudioPlatform.Properties;
+using UBTStandardLibrary.Dialog;
+
+namespace StudioPlatform
+{
+    public partial class MainWindow : UBTStandardLibrary.Forms.N1Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Cef.Initialize(new CefSettings() {MultiThreadedMessageLoop = false});
+            Cef.DoMessageLoopWork();
+            CheckForIllegalCrossThreadCalls = false;
+            KeyPreview = true;
+            pnlBrowsers.KeyDown += OnKeyDown;
+            this.KeyDown += OnKeyDown;
+            for (int x = 0; x < pnlBrowsers.RowCount; x++)
+            {
+                for (int y = 0; y < pnlBrowsers.ColumnCount; y++)
+                {
+                    ChromiumWebBrowser browser = new ChromiumWebBrowser("https://www.n1developer.com");
+                    browser.Dock = DockStyle.Fill;
+                    browser.Padding = new Padding(5);
+                    pnlBrowsers.Controls.Add(browser, y, x);
+                }
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.Shift && e.Alt && e.KeyCode == Keys.A)
+            {
+                InputDialog dialog = new InputDialog("Enter Password", new List<Input>(){new Input()
+                {
+                    Hint = "Password",
+                    Index = 0,
+                    Type = InputType.Password,
+                    Name = "p"
+                }}, this);
+
+                if (dialog.ShowIt() == ResultType.OK)
+                {
+                    string data = dialog.Output.Find(x => x.Name.Equals("p")).Value;
+                    if (data.Equals("n1developer"))
+                    {
+                        string jsonData = Settings.Default.JsonData;
+
+                        List<Input> inp;
+
+                        if (jsonData.Trim().Equals(""))
+                        {
+                            inp = new List<Input>();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class Setting
+    {
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+}
